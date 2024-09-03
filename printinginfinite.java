c7a0413b9bc8f8b2a87ac659a194f919d614dadc
Adel-18 <md.adel88700@gmail.com>
2024-09-03 12:20:05 +0530

    Add files via upload

---
+++ printinginfinite.java
@@ -0,0 +1,19 @@
+package day1;
+import java.util.Scanner;
+public class printinginfinite {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+		int i=0;
+		while(i<=n)
+		{
+			System.out.println(i);
+			i++;
+			
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+}
